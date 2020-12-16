514aeb5b02b8b448e5eb0adc8dfee491c9091d1a
djilou <ncdjilou@gmail.com>
2020-12-17 00:03:35 +0100

    style about page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,58 @@
 import React from "react";
-import home1 from "../img/home1";
+import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
           <div className="hide">
             <h2>We work to make</h2>
           </div>
           <div className="hide">
             <h2>
-              Your <span>dreams</span>come
+              Your <span>dreams</span> come
             </h2>
           </div>
           <div className="hide">
             <h2>true.</h2>
           </div>
         </div>
-        <div className="image">
-            <img src={home1} alt="Guy with camera for now"/>
-        </div>
-      </div>
-      <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem impedit praesentium ex dignissimos. Fugiat qui ullam vero voluptatibus neque.
-      </p>
-      <button>Contact Us</button>
-    </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          voluptatem impedit praesentium ex dignissimos.
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="Guy with camera for now" />
+      </Image>
+    </About>
   );
 };
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-b etween;
+  padding: 3rem 7rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
 
+const Image = styled.div`
+  flex: 1;
+  img {
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+  }
+`;
 export default AboutSection;
--- src/pages/About.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AboutSection from "../components/AboutSection";
-
-const About = () => {
-    return (
-        <AboutSection />
-    )
-};
-export default About;
